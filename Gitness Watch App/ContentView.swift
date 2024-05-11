6090139403410263403f7364b6a2b0df24cbf06e
Kiwi Guo <loshochung@hotmail.com>
2024-05-11 10:47:35 +0800

    Initial Commit

---
+++ Gitness Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gitness Watch App
+//
+//  Created by Kiwi Guo on 2024/5/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gitness Watch App/GitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitnessApp.swift
+//  Gitness Watch App
+//
+//  Created by Kiwi Guo on 2024/5/11.
+//
+
+import SwiftUI
+
+@main
+struct Gitness_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
